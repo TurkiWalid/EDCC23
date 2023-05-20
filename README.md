a4d28ceee97225936db2396c0ddd5586ebb7d276
walid <walid.turki.94@gmail.com>
2023-05-21 00:26:07 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# iOS Architect Crash Course • May 15th-21st 2023 • EssentialDeveloper.com
+
+https://www.essentialdeveloper.com/ios-architect-crash-course/may-2023-e03b
+
+It's time to put your skills to the test!
+
+This is the project used in the iOS Architect Crash Course lectures.
+
+Watch the lectures and implement what you learned into this project to practice applying architectural patterns while refactoring legacy code into clean code with clean architecture.
+
+---
+
+## Instructions
+
+1) Open the `iACC.xcodeproj` project on Xcode 14.x or higher.
+
+	- Older Xcode versions are not supported.
+	- Beta Xcode versions are not supported.
+
+2) The project already comes with a comprehensive suite of automated tests. Throughout the refactoring, run all tests with CMD+U after changing the code.
+
+3) Commit every time you make a change, and the tests pass. This way, you have a working state to revert to if needed.
+
+	- If a test fails, a behavior of the system is broken. Revert your changes to the previous commit with all tests passing and try again.
+
+4) The project should build without warnings.
+
+5) The code should be carefully organized and easy to read (e.g., indentation must be consistent).
+
+Happy coding!
---
+++ iACC/ListViewController.swift
@@ -0,0 +1,268 @@
+//
+// Copyright © Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+	var items = [ItemViewModel]()
+	
+	var retryCount = 0
+	var maxRetryCount = 0
+	var shouldRetry = false
+	
+	var longDateStyle = false
+	
+	var fromReceivedTransfersScreen = false
+	var fromSentTransfersScreen = false
+	var fromCardsScreen = false
+	var fromFriendsScreen = false
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		refreshControl = UIRefreshControl()
+		refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+		
+		if fromFriendsScreen {
+			shouldRetry = true
+			maxRetryCount = 2
+			
+			title = "Friends"
+			
+			navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
+			
+		} else if fromCardsScreen {
+			shouldRetry = false
+			
+			title = "Cards"
+			
+			navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addCard))
+			
+		} else if fromSentTransfersScreen {
+			shouldRetry = true
+			maxRetryCount = 1
+			longDateStyle = true
+
+			navigationItem.title = "Sent"
+			navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(sendMoney))
+
+		} else if fromReceivedTransfersScreen {
+			shouldRetry = true
+			maxRetryCount = 1
+			longDateStyle = false
+			
+			navigationItem.title = "Received"
+			navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Request", style: .done, target: self, action: #selector(requestMoney))
+		}
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		if tableView.numberOfRows(inSection: 0) == 0 {
+			refresh()
+		}
+	}
+	
+	@objc private func refresh() {
+		refreshControl?.beginRefreshing()
+		if fromFriendsScreen {
+			FriendsAPI.shared.loadFriends { [weak self] result in
+				DispatchQueue.mainAsyncIfNeeded {
+					self?.handleAPIResult(result)
+				}
+			}
+		} else if fromCardsScreen {
+			CardAPI.shared.loadCards { [weak self] result in
+				DispatchQueue.mainAsyncIfNeeded {
+					self?.handleAPIResult(result)
+				}
+			}
+		} else if fromSentTransfersScreen || fromReceivedTransfersScreen {
+			TransfersAPI.shared.loadTransfers { [weak self] result in
+				DispatchQueue.mainAsyncIfNeeded {
+					self?.handleAPIResult(result)
+				}
+			}
+		} else {
+			fatalError("unknown context")
+		}
+	}
+    
+    private func handleAPIResult(_ result: Result<[Friend], Error>) {
+        let selectionFriend = { [weak self](friend: Friend) in
+            let vc = FriendDetailsViewController()
+            vc.friend = friend
+            self?.show(vc, sender: self)
+        }
+        switch result {
+        case let .success(friends):
+            if User.shared?.isPremium == true {
+                (UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.save(items as! [Friend])
+            }
+            self.retryCount = 0
+            self.items = friends.map{ friend in
+                return ItemViewModel(friend: friend, selection: { selectionFriend(friend)})
+            }
+            self.refreshControl?.endRefreshing()
+            self.tableView.reloadData()
+        case let .failure(error):
+            if User.shared?.isPremium == true {
+               (UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.loadFriends { [weak self] result in
+                   DispatchQueue.mainAsyncIfNeeded {
+                       switch result {
+                       case let .success(friends):
+                           self?.items = friends.map{ friend in
+                               return ItemViewModel(friend: friend, selection: { selectionFriend(friend)})
+                           }
+                           self?.tableView.reloadData()
+                           
+                       case let .failure(error):
+                           self?.showError(error)
+                       }
+                       self?.refreshControl?.endRefreshing()
+                   }
+               }
+            }else {
+                self.showError(error)
+                self.refreshControl?.endRefreshing()
+            }
+        break
+        }
+    }
+    
+    private func handleAPIResult(_ result: Result<[Card], Error>) {
+        switch result {
+        case let .success(cards):
+            self.retryCount = 0
+            self.items = cards.map{ card in
+                return ItemViewModel(card: card, selection: { [weak self] in
+                let vc = CardDetailsViewController()
+                vc.card = card
+                self?.show(vc, sender: self)})
+            }
+            self.refreshControl?.endRefreshing()
+            self.tableView.reloadData()
+        case let .failure(error):
+            self.showError(error)
+            self.refreshControl?.endRefreshing()
+            break
+        }
+    }
+    
+    private func handleAPIResult(_ result: Result<[Transfer], Error>) {
+        switch result {
+        case let .success(transfers):
+            self.retryCount = 0
+            var filteredItems = transfers
+            if fromSentTransfersScreen {
+                filteredItems = transfers.filter(\.isSender)
+            } else {
+                filteredItems = transfers.filter { !$0.isSender }
+            }
+            self.items = filteredItems.map{ transfer in
+                return ItemViewModel(transfer: transfer, longDateStyle: self.longDateStyle, selection: { [weak self] in
+                    let vc = TransferDetailsViewController()
+                    vc.transfer = transfer
+                    self?.show(vc, sender: self)
+                })
+            }
+            self.refreshControl?.endRefreshing()
+            self.tableView.reloadData()
+        case let .failure(error):
+            self.showError(error)
+            self.refreshControl?.endRefreshing()
+            break
+        }
+    }
+	
+    private func showError(_ error: Error){
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        showDetailViewController(alert, sender: self)
+    }
+    
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		1
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		items.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let item = items[indexPath.row]
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "ItemCell")
+		cell.configure(item)
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let item = items[indexPath.row]
+        item.selection()
+	}
+	
+	@objc func addCard() {
+        show(AddCardViewController(), sender: self)
+	}
+	
+	@objc func addFriend() {
+        show(AddFriendViewController(), sender: self)
+	}
+	
+	@objc func sendMoney() {
+		show(SendMoneyViewController(), sender: self)
+	}
+	
+	@objc func requestMoney() {
+		show(RequestMoneyViewController(), sender: self)
+	}
+}
+
+struct ItemViewModel {
+    var labelText: String
+    var detailText: String
+    var selection: ()->Void
+    
+    init(friend: Friend, selection: @escaping () -> Void) {
+        labelText = friend.name
+        detailText = friend.phone
+        self.selection = selection
+    }
+    
+    init(card: Card, selection: @escaping () -> Void){
+        labelText = card.number
+        detailText = card.holder
+        self.selection = selection
+    }
+    
+    init(transfer: Transfer, longDateStyle: Bool, selection: @escaping () -> Void) {
+        let numberFormatter = Formatters.number
+        numberFormatter.numberStyle = .currency
+        numberFormatter.currencyCode = transfer.currencyCode
+        
+        let amount = numberFormatter.string(from: transfer.amount as NSNumber)!
+        labelText = "\(amount) • \(transfer.description)"
+        
+        let dateFormatter = Formatters.date
+        if longDateStyle {
+            dateFormatter.dateStyle = .long
+            dateFormatter.timeStyle = .short
+            detailText = "Sent to: \(transfer.recipient) on \(dateFormatter.string(from: transfer.date))"
+        } else {
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            detailText = "Received from: \(transfer.sender) on \(dateFormatter.string(from: transfer.date))"
+        }
+        self.selection = selection
+    }
+}
+
+
+extension UITableViewCell {
+    func configure(_ itemVm: ItemViewModel) {
+        textLabel?.text = itemVm.labelText
+        detailTextLabel?.text = itemVm.detailText
+    }
+}
